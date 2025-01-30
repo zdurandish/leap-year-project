d07f95a5365b37d44bc54cda9b94523275126156
zahra <z.durandish52@gmail.com>
2025-01-30 08:36:54 -0800

    leap year function defined

---
+++ task.py
@@ -0,0 +1,30 @@
+chosen_year = int(input("Enter the year: "))
+
+def is_leap_year(year):
+    if not year % 4 == 0:
+        return False
+    else:
+        if not year % 100 == 0:
+            return True
+        else:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+
+print(is_leap_year(chosen_year))
+
+# a second way to define the function:
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+print(is_leap_year(chosen_year))
